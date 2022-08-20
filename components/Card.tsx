85fb0d1594213be6e6ed6b4961a2656b77c5405e
Jones Fernandes <keysijones02@gmail.com>
2022-08-19 22:17:52 -0300

    Adds internal margin to separate card title from description

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,8 +8,14 @@ type Props = {
 
 const Card = ({ link, name, description }: Props) => (
   <Link href={link}>
-    <div className='hover:scale-110 transition-all bg-white cursor-grabbing rounded-lg [text-decoration:none] text-left p-6 m-4 min-w-[300px]'>
-      <h2 className='text-2xl font-medium sm:text-3xl'>{name} &rarr;</h2>
+    <div className='hover:scale-[1.08] transition-all bg-white cursor-grabbing rounded-lg [text-decoration:none] text-left p-6 m-4 min-w-[300px]'>
+      <h2
+        className={`text-2xl ${
+          description ? 'font-medium' : 'font-normal'
+        } sm:text-3xl mb-2`}
+      >
+        {name} &rarr;
+      </h2>
       {description && <p className='text-lg sm:text-xl'>{description}</p>}
     </div>
   </Link>
