dfe2bf72ff86c666095761ba99cf6a1846516cec
Jones Fernandes <keysijones02@gmail.com>
2022-08-19 22:09:30 -0300

    Adds smooth transition to card when hovering

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Card = ({ link, name, description }: Props) => (
   <Link href={link}>
-    <div className='hover:border-blue-400 focus:border-blue-400 active:border-blue-400 bg-white cursor-grabbing rounded-lg [text-decoration:none] text-left p-6 m-4 min-w-[300px]'>
+    <div className='hover:scale-110 transition-all bg-white cursor-grabbing rounded-lg [text-decoration:none] text-left p-6 m-4 min-w-[300px]'>
       <h2 className='text-2xl font-medium sm:text-3xl'>{name} &rarr;</h2>
       {description && <p className='text-lg sm:text-xl'>{description}</p>}
     </div>
